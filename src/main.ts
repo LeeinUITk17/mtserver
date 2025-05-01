577208da7177ad2c46ffe172263630ba5e156b4c
LeeinUITk17 <22521276@gm.uit.edu.vn>
2025-05-01 22:40:22 +0700

    feat: done set up cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import passport from 'passport';
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    next();
+  });
+
+  app.use(passport.initialize());
+
+  const port = 8000;
+  await app.listen(port, '0.0.0.0');
+  Logger.log(`Server is running on http://localhost:${port}`);
 }
+
 bootstrap();
